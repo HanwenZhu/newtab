4a23c872861ef9b5649f9492113ceae4f19f1b58
HanwenZhu <thomas.zhu.sh@hotmail.com>
2019-09-30 23:09:20 +0800

    1. Refactor `models.py` and add an (although unused) `Login` class; 2. Change some of the JS and JSON of how the clock works; 3. Of course, add the extra requirements from `Login`

--- newtab/static/scripts/newtab.js
+++ newtab/static/scripts/newtab.js
@@ -2,21 +2,21 @@ var interval;
 
 
 function updateStatus() {
-    return $.getJSON("/status").done(response => {
+    return $.getJSON('/status', response => {
         $('#time-today').html(response.today);
-        $("#m1").html(response.mdHMS.charAt(0));
-        $("#m2").html(response.mdHMS.charAt(1));
-        $("#d1").html(response.mdHMS.charAt(2));
-        $("#d2").html(response.mdHMS.charAt(3));
-        $("#time-day").html(response.day);
-        $("#H1").html(response.mdHMS.charAt(4));
-        $("#H2").html(response.mdHMS.charAt(5));
-        $("#M1").html(response.mdHMS.charAt(6));
-        $("#M2").html(response.mdHMS.charAt(7));
-        $("#S1").html(response.mdHMS.charAt(8));
-        $("#S2").html(response.mdHMS.charAt(9));
-        $("#status-room").html(response.room);
-        $("#status-activity").html(response.activity);
+        $('#m1').html(response.mdHMS.charAt(0));
+        $('#m2').html(response.mdHMS.charAt(1));
+        $('#d1').html(response.mdHMS.charAt(2));
+        $('#d2').html(response.mdHMS.charAt(3));
+        $('#time-day').html(response.day);
+        $('#H1').html(response.mdHMS.charAt(4));
+        $('#H2').html(response.mdHMS.charAt(5));
+        $('#M1').html(response.mdHMS.charAt(6));
+        $('#M2').html(response.mdHMS.charAt(7));
+        $('#S1').html(response.mdHMS.charAt(8));
+        $('#S2').html(response.mdHMS.charAt(9));
+        $('#status-room').html(response.room);
+        $('#status-activity').html(response.activity);
     });
 }
 
