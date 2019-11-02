07fe20e6eb004468614fee65b470179117f810d0
HanwenZhu <thomas.zhu.sh@hotmail.com>
2019-11-02 12:06:06 +0800

    Cleaner code

--- newtab/static/scripts/newtab.js
+++ newtab/static/scripts/newtab.js
@@ -4,6 +4,18 @@
 
 var interval;
 
+var $time_today, $time_day, $status_room, $status_activity, $mdHMS;
+
+// We need videos with an alpha channel for transparency.
+// I found that *.mov and *.webm work.
+// Newest Safari does not accept WebM (although WebM should be supported
+// as per the HTML5 <video> standard).  But (since it's Apple's) it supports
+// Quicktime's *.mov.
+// Chrome does support WebM.  It (reasonably) doesn't support *.mov.
+var videoElement = $('<video></video>')[0];
+var webm = videoElement.canPlayType('video/webm');
+var quicktime = videoElement.canPlayType('video/quicktime');
+
 
 function fetchCachedVideo(filename, callback) {
     // As was discussed, the best practice is to cache individual videos.
@@ -44,41 +56,33 @@ function fetchCachedVideo(filename, callback) {
 function setup() {
     // Disable cache
     $.ajaxSetup({cache: false});
+
+    $time_today = $('#time-today');
+    $time_day = $('#time-day');
+    $status_room = $('#status-room');
+    $status_activity = $('#status-activity');
+    $mdHMS = $('.mdHMS')
     $.getJSON('/status').done(response => {
-        $('#time-today').html(response.today);
-        $('#m1 > .number').html(response.mdHMS.charAt(0));
-        $('#m2 > .number').html(response.mdHMS.charAt(1));
-        $('#d1 > .number').html(response.mdHMS.charAt(2));
-        $('#d2 > .number').html(response.mdHMS.charAt(3));
-        $('#time-day').html(response.day);
-        $('#H1 > .number').html(response.mdHMS.charAt(4));
-        $('#H2 > .number').html(response.mdHMS.charAt(5));
-        $('#M1 > .number').html(response.mdHMS.charAt(6));
-        $('#M2 > .number').html(response.mdHMS.charAt(7));
-        $('#S1 > .number').html(response.mdHMS.charAt(8));
-        $('#S2 > .number').html(response.mdHMS.charAt(9));
-        $('#status-room').html(response.room);
-        $('#status-activity').html(response.activity);
+        $time_today.html(response.today);
+        $time_day.html(response.day);
+        $status_room.html(response.room);
+        $status_activity.html(response.activity);
+        $mdHMS.each((index, digit) => {
+            $(digit).find('.number').html(response.mdHMS.charAt(index));
+        });
     });
 }
 
 
 function updateStatus() {
     $.getJSON('/status').done(response => {
-        $('#time-today').html(response.today);
-        manimTransform($('#m1'), response.mdHMS.charAt(0));
-        manimTransform($('#m2'), response.mdHMS.charAt(1));
-        manimTransform($('#d1'), response.mdHMS.charAt(2));
-        manimTransform($('#d2'), response.mdHMS.charAt(3));
-        $('#time-day').html(response.day);
-        manimTransform($('#H1'), response.mdHMS.charAt(4));
-        manimTransform($('#H2'), response.mdHMS.charAt(5));
-        manimTransform($('#M1'), response.mdHMS.charAt(6));
-        manimTransform($('#M2'), response.mdHMS.charAt(7));
-        manimTransform($('#S1'), response.mdHMS.charAt(8));
-        manimTransform($('#S2'), response.mdHMS.charAt(9));
-        $('#status-room').html(response.room);
-        $('#status-activity').html(response.activity);
+        $time_today.html(response.today);
+        $time_day.html(response.day);
+        $status_room.html(response.room);
+        $status_activity.html(response.activity);
+        $mdHMS.each((index, digit) => {
+            manimTransform($(digit), response.mdHMS.charAt(index));
+        });
     });
 }
 
@@ -87,18 +91,9 @@ function manimTransform($element, target) {
     var $number = $element.find('.number');
     var $transform = $element.find('video.transform');
 
-    if ($number.text() === target) {
+    if ($number.html() === target) {
         return;
     }
-
-    // We need videos with an alpha channel for transparency.
-    // I found that *.mov and *.webm work.
-    // Newest Safari does not accept WebM (although WebM should be supported
-    // as per the HTML5 <video> standard).  But (since it's Apple's) it supports
-    // Quicktime's *.mov.
-    // Chrome does support WebM.  It (reasonably) doesn't support *.mov.
-    var webm = $transform[0].canPlayType('video/webm');
-    var quicktime = $transform[0].canPlayType('video/quicktime');
 
     if (!webm && !quicktime) {
         $number.html(target);
