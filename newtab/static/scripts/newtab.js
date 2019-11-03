7bdd0df656f9876c11e177f7d63265dfb5296edb
HanwenZhu <thomas.zhu.sh@hotmail.com>
2019-11-03 15:08:36 +0800

    Reverting the manim transforms. Reasons: the video thing still isn't fixed: HTML5 video loading issues cause them to randomly unload; Trying to fix these result in a high power consumption rate (even when not fixing the rate is still high) (see issue #1); I have explored a lot of other alternative solutions, to no avail (e.g. the GIF does not support an alpha channel, and forcing the background to #eeeeee still makes it blurry and obvious in the foreground; the only video options are WebM and Quicktime with alpha channels; Just don't even think about flash; Canvas might be worth exploring in the future); Slows down the web loading and leads to occasional glitches. For now, just focus on other features more useful for this application.

--- newtab/static/scripts/newtab.js
+++ newtab/static/scripts/newtab.js
@@ -1,44 +1,9 @@
 // TODO, jQuery is slow
-// TODO, video is not clear.  Consider doubling width & height?
-// TODO, support other browsers?
 
 var interval;
 
 // To avoid DOM lookup
 var $timeToday, $timeDay, $statusRoom, $statusActivity, $mdHMS;
-
-// We need videos with an alpha channel for transparency.
-// I found that *.mov and *.webm work.
-// Newest Safari does not accept WebM (although WebM should be supported
-// as per the HTML5 <video> standard).  But (since it's Apple's) it supports
-// Quicktime's *.mov.
-// Chrome does support WebM.  It (reasonably) doesn't support *.mov.
-var videoElement = $('<video></video>')[0];
-var webm = videoElement.canPlayType('video/webm');
-var quicktime = videoElement.canPlayType('video/quicktime');
-var extension = webm ? '.webm' : quicktime ? '.mov' : null;
-
-var videoPool = new Array(10).fill(null).map(item => new Array(10).fill(null));
-
-
-function fetchVideo(from, to, callback) {
-    // As was discussed, the best practice is to cache individual videos.
-    // This will be implemented by appending new videos into an array.
-    // There is the alternative of having a long video cached and jumping to
-    // individual timestamps, but it is not efficient for this task since
-    // 01, 12, ..., 90 are used predominantly.
-
-    if (videoPool[from][to] !== null) {
-        return videoPool[from][to];
-    }
-
-    var $video = $('<video class="transform" src="/static/videos/' +
-                   `${from}${to}${extension}" preload="auto" muted ` +
-                   'playsinline disablepictureinpicture>');
-    $video[0].load();
-    videoPool[from][to] = $video;
-    return $video;
-}
 
 
 function setup() {
@@ -50,17 +15,8 @@ function setup() {
     $statusRoom = $('#status-room');
     $statusActivity = $('#status-activity');
     $mdHMS = $('.mdHMS');
-    $.getJSON('/status').done(response => {
-        $timeToday.html(response.today);
-        $timeDay.html(response.day);
-        $statusRoom.html(response.room);
-        $statusActivity.html(response.activity);
-        $mdHMS.each((index, digit) => {
-            number = parseInt(response.mdHMS.charAt(index));
-            $(digit).html(number);
-            digit.number = number;
-        });
-    });
+
+    update();
 }
 
 
@@ -71,30 +27,8 @@ function updateStatus() {
         $statusRoom.html(response.room);
         $statusActivity.html(response.activity);
         $mdHMS.each((index, digit) => {
-            manimTransform($(digit), response.mdHMS.charAt(index));
+            digit.innerHTML = response.mdHMS.charAt(index);
         });
-    });
-}
-
-
-function manimTransform($element, target) {
-    target = parseInt(target);
-    if ($element[0].number === target) {
-        return;
-    }
-
-    if (!webm && !quicktime) {
-        $element[0].number = target;
-        $element.html(target);
-        return;
-    }
-
-    var $video = fetchVideo($element[0].number, target);
-    $video.one('ended', () => {
-        $element.html(target);
-    })[0].play().then(() => {
-        $element.html($video);
-        $element[0].number = target;
     });
 }
 
