29bf8ef9c212845db8857a734df935e283123321
HanwenZhu <thomas.zhu.sh@hotmail.com>
2019-11-26 20:01:24 +0800

    Update weather API backend

--- newtab/weather.py
+++ newtab/weather.py
@@ -1,4 +1,5 @@
 import multiprocessing
+import re
 import time
 
 import requests
@@ -6,23 +7,27 @@ import requests
 
 # A very good API
 URL = 'http://wttr.in/?format=%22%t+%c%22'
+# r'..' since a unicode emoji takes up two bytes
+WTTR_IN_RE = re.compile(r'"(\+|-)[0-9]+°C .."\n')
 REFRESH_RATE = 1800
 
 
 def _update_weather(status_queue):
     while True:
         try:
-            response = requests.get(URL)
+            response = requests.get(URL, allow_redirects=False)
         except requests.exceptions.ConnectionError:
-            pass
+            time.sleep(120)
+            continue
         else:
             response.encoding = 'utf-8'
-            status_queue.get()
-            status_queue.put(
-                # If the temperature starts with a plus sign
-                response.text.lstrip('"+').rstrip('"\n').replace('C', '')
-            )
-        finally:
+            text = response.text
+            if response.status_code == 200 and WTTR_IN_RE.fullmatch(text):
+                status_queue.get()
+                status_queue.put(
+                    # If the temperature starts with a plus sign
+                    text.lstrip('"+').rstrip('"\n').replace('C', '')
+                )
             time.sleep(REFRESH_RATE)
 
 
