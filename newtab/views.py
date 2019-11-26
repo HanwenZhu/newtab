be2f8c598f769d7e5a8024e957ce62da02ef2683
HanwenZhu <thomas.zhu.sh@hotmail.com>
2019-11-26 21:02:59 +0800

    Completed the wifi module

--- newtab/views.py
+++ newtab/views.py
@@ -16,3 +16,38 @@ def clock():
 @newtab.app.route('/weather')
 def weather():
     return jsonify(newtab.weather.status())
+
+
+@newtab.app.route('/wifi/wifi')
+def wifi_wifi():
+    return jsonify(newtab.wifi.wifi())
+
+
+@newtab.app.route('/wifi/ip')
+def wifi_ip():
+    return jsonify(newtab.wifi.ip())
+
+
+@newtab.app.route('/wifi/hostname')
+def wifi_hostname():
+    return jsonify(newtab.wifi.hostname())
+
+
+@newtab.app.route('/wifi/mac')
+def wifi_mac():
+    return jsonify(newtab.wifi.mac())
+
+
+@newtab.app.route('/wifi/google')
+def wifi_google():
+    return jsonify(newtab.wifi.google_connectivity())
+
+
+@newtab.app.route('/wifi/baidu')
+def wifi_baidu():
+    return jsonify(newtab.wifi.baidu_connectivity())
+
+
+@newtab.app.route('/wifi/login')
+def wifi_login():
+    return jsonify(newtab.wifi.login())
--- newtab/wifi.py
+++ newtab/wifi.py
@@ -1,15 +1,93 @@
+import base64
+import json
+import os
+import socket
+import subprocess
+import sys
 import time
+import uuid
 
 import requests
 
+import newtab
 
-def _do_encrypt_rc4(source, password):
+
+def google_connectivity(timeout=5):
+    try:
+        response = requests.head('http://google.com', timeout=timeout)
+    except requests.ConnectionError:
+        return False
+    else:
+        return response.elapsed.total_seconds()
+
+
+def baidu_connectivity(timeout=5):
+    try:
+        response = requests.head('http://baidu.com', timeout=timeout)
+    except requests.ConnectionError:
+        return False
+    else:
+        return response.elapsed.total_seconds()
+
+
+def ip():
+    return socket.gethostbyname(hostname())
+
+
+def hostname():
+    return socket.gethostname()
+
+
+def mac():
+    return ':'.join(f'{byte:02x}'
+                    for byte in uuid.getnode().to_bytes(6, 'big'))
+
+
+def wifi():
+    if sys.platform != 'darwin':
+        return False  # TODO
+    commands = ['networksetup', '-getairportnetwork', 'en0']
+    output = subprocess.check_output(commands, shell=False).decode()
+    if output.startswith('You are not associated with an AirPort network.\n'):
+        return False
+    return output.split()[-1]
+
+
+def login():
+    creds_filename = os.path.join(newtab.app.instance_path, 'wifi-creds.json')
+    if not os.path.isfile(creds_filename):
+        return False
+    if wifi() not in {'SJWIRELESS', 'STUWIRELESS'}:
+        return False
+
+    with open(creds_filename) as file:
+        credentials = json.load(file)
+    password = base64.b64decode(credentials['password']).decode()
+    rckey = str(int(time.time() * 1000))
+    pwd = _do_encrypt_rc4(password, rckey)
+    params = {
+        'opr': 'pwdLogin',
+        'userName': credentials['username'],
+        'pwd': pwd,
+        'rc4Key': rckey,
+        'rememberPwd': '1'
+    }
+    response = requests.post('http://1.1.1.3/ac_portal/login.php', data=params)
+    response.encoding = 'utf-8'
+    status = json.loads(response.text.replace("'", '"'))
+    logged_in = '用户已在线，不需要再次认证'
+    return status.get('success') or status.get('msg') == logged_in
+
+
+def _do_encrypt_rc4(source, raw_key):
+    # Literal line-by-line translation of the original do_encrypt_rc4
+    # JavaScript function of the login page
     source = source.strip()
 
     key = []
     sbox = []
     for i in range(256):
-        key.append(ord(password[i % len(password)]))
+        key.append(ord(raw_key[i % len(raw_key)]))
         sbox.append(i)
 
     j = 0
@@ -29,16 +107,3 @@ def _do_encrypt_rc4(source, password):
         output.append(out)
 
     return ''.join(output)
-
-
-def login(username, password):
-    rc4key = str(int(time.time() * 1000))
-    pwd = _do_encrypt_rc4(password, rc4key)
-    params = {
-        'opr': 'pwdLogin',
-        'userName': username,
-        'pwd': pwd,
-        'rc4key': rc4key,
-        'rememberPwd': '1'
-    }
-    requests.post('http://1.1.1.3/ac_portal/login.php', data=params)
