950b95015bf5d5d23d216081f23c20d2121d1bc2
hanwenzhu <thomas.zhu.sh@hotmail.com>
2020-04-15 09:21:01 +0800

    temporary debug-ish solution to the 3d cylinder problem, very laggy

--- newtab/static/scripts/station.js
+++ newtab/static/scripts/station.js
@@ -1,10 +1,16 @@
-var smoothness = 10;
+var smoothness = 12;
 var angles = new Array(smoothness).fill(null).map((element, index) => index / smoothness);
 
 $(document).ready(() => {
     $('.cylinder').each((index, cylinder) => {
-        angles.forEach(?? => {
-            $(cylinder).clone().css()
+        angles.forEach(angle => {
+            var diameter = $(cylinder).width();
+            var facetWidth = diameter * Math.tan(Math.PI / smoothness);
+            $('<div></div>').addClass('abs-center facet').css({
+                width: `${facetWidth}px`,
+                transform: `rotateY(${angle}turn) translateZ(${diameter / 2}px)`,
+                background: 'linear-gradient(to right, red, blue)'
+            }).appendTo($(cylinder));
         });
     });
 });
